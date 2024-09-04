8d1d89a2bc5bcdc2652da0d79c78e706d040860b
JoshuaBaraka24 <joshua.adalo@strathmore.edu>
2024-09-04 16:04:37 +0300

    Edit 6

---
+++ Menus/menus.php
@@ -0,0 +1,10 @@
+<?php
+class menus{
+    public function main_menu(){
+        ?>
+        <div>
+            <a href="./">Home</a>
+            <a href="./">About us</a>
+            <?php
+    }
+}
--- load.php
+++ load.php
@@ -1,5 +1,29 @@
 <?php
-require_once "fnc.php";
+require_once "Layouts/layouts.php";
+$ObjLayouts = new layouts();
+require_once "Menus/menus.php";
+$ObjMenus = new menus();
 
-$Obj = new fnc();
+//$arr = ["black", "white", "green", "red"];
+
+//foreach($arr AS $color){
+ //   print $color . "<br>";
+//}
+
+//print dirname(__FILE__);
+//print "<br>";
+//print "<br>";
+//print $_SERVER["PHP_SELF"];
+//print "<br>";
+//print "<br>";
+//print basename($_SERVER["PHP_SELF"]);
+//print "<br>";
+//print "<br>";
+
+//if(file_exists("index.php")){
+//    print "yes";
+//}
+//else{
+//    print "no";
+//}
 ?>
