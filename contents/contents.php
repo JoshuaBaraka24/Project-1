3ada81ec8e1e042be8a0c6407d5d9ca9161e367c
JoshuaBaraka24 <joshua.adalo@strathmore.edu>
2024-09-11 16:03:54 +0300

    Edit 9

--- contents/contents.php
+++ contents/contents.php
@@ -23,10 +23,7 @@ class contents{
         ?>
         <div class ="side_bar">
         <h1>Side Bar</h1>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio rerum quos sit. Mollitia minima molestiae vitae officia aspernatur vero exercitationem nobis. Expedita assumenda quam harum vero, culpa numquam ab animi? 
-               Lorem ipsum dolor sit amet consectetur adipisicing elit. Id vitae, voluptates quidem est vero harum ipsam praesentium inventore alias dolor, architecto tenetur recusandae quia. Exercitationem voluptatum nobis odio provident cupiditate.
-               Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat, commodi praesentium. Provident ad nobis deleniti nesciunt dignissimos, architecto nostrum rerum reiciendis, corrupti maiores quis cum nisi inventore repellendus eius molestias!
-             </p>
+            <p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?</p>
 
     </div>
     </div>
