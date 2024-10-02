5482d87eca05140fad5bbbf14737b1f0734cfdfd
JoshuaBaraka24 <joshua.adalo@strathmore.edu>
2024-10-02 15:21:19 +0300

    Implemented clean data case on input

--- load.php
+++ load.php
@@ -1,13 +1,14 @@
 <?php
-
+session_start();
 require "includes/constants.php";
 require "includes/dbConnection.php";
+require "lang/en.php";
 
 // Class Auto Load 
 
 function classAutoLoad($classname){
 
-    $directories = ["contents", "layouts", "menus", "forms" , "processes" ];
+    $directories = ["contents", "layouts", "menus", "forms", "processes", "global"];
 
     foreach($directories AS $dir){
         $filename = dirname(__FILE__) . DIRECTORY_SEPARATOR . $dir . DIRECTORY_SEPARATOR . $classname . ".php";
@@ -19,6 +20,10 @@ function classAutoLoad($classname){
 
 spl_autoload_register('classAutoLoad');
 
+
+    $ObjGlob = new fncs();
+    $ObjSendMail = new SendMail();
+
 // Create instances of all classes
     $ObjLayouts = new layouts();
     $ObjMenus = new menus();
@@ -27,28 +32,9 @@ spl_autoload_register('classAutoLoad');
     $ObjForm = new user_forms();
     $conn = new dbConnection(DBTYPE, HOSTNAME, DBPORT, HOSTUSER, HOSTPASS, DBNAME);
 
+
 // Create process instances
 
-$ObjAuth = new auth();
-$ObjAuth->signup($conn);
-/*
-require "includes/constants.php";
-require "includes/dbConnection.php";
+    $ObjAuth = new auth();
+    $ObjAuth->signup($conn, $ObjGlob, $ObjSendMail, $lang);
 
-$conn = new dbConnection(DBTYPE, HOSTNAME, DBPORT, HOSTUSER, HOSTPASS, DBNAME);
-*/
-
-// print 
-// print "<br>";
-// print "<br>";
-// print $_SERVER["PHP_SELF"];
-// print "<br>";
-// print "<br>";
-// print basename($_SERVER["PHP_SELF"]);
-// print "<br>";
-// print "<br>";
-// if(file_exists("index.php") AND is_readable("index.php")){
-//     print "yes";
-// }else{
-//     print "no";
-// }
