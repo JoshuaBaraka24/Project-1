c293dc9a5eccfd418cafb99db01bff980f4b8464
JoshuaBaraka24 <joshua.adalo@strathmore.edu>
2024-10-16 21:20:04 +0300

    heading display change

--- contents/headings.php
+++ contents/headings.php
@@ -4,9 +4,9 @@ class headings{
         ?>
       <div class="p-5 mb-4 bg-body-tertiary rounded-3">
         <div class="container-fluid py-2">
-          <h1 class="display-5 fw-bold">Assignment II</h1>
-          <p class="col-md-12 fs-6">This is an individual assignment. We assume that you already know how to set up a localhost server(s): & MySQL database and Apache web servers.<br>If not, your instructor will assist you to set it up.</p>
-          <button class="btn btn-primary btn-lg" type="button">Example button</button>
+          <h1 class="display-5 fw-bold">QUICKBITE</h1>
+          <p class="col-md-12 fs-6">Join us and be a part of this dedicated group of engineers to develop what WE like to call, PROJECT II.<br>Feel free to join as we are  recruiting new members in this esteemed journey.</p>
+          <a href="signup.php"> <button class="btn btn-primary btn-lg" type="button">Join us</button></a>
         </div>
       </div>
         <?php
